e3480c97b40378f675771c5ce0200ef65d6eadf0
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-12-20 21:47:11 +0530

    Add fevicon

--- src/components/PokeArc.vue
+++ src/components/PokeArc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="container">
     <h1>{{ msg }}</h1>
 	<md-button v-on:click='getFighters()'>
       Magic
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/PokeArc'
+import PokeArc from '@/components/PokeArc'
 
 Vue.use(Router)
 
@@ -11,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'PokeArc',
-      component: HelloWorld
+      component: PokeArc
     }
   ]
 })
