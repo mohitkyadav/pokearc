4fb7710f84ef1f2bc479db6e27bbeb1b74f3a267
Alex Howes <alex@alexhowes.co.uk>
2018-10-04 20:46:58 +0100

    Half way there, added use light theme setting and push it to localstorage.

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import VueResource from 'vue-resource'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default-dark.css'
+// import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
 Vue.use(VueResource)
